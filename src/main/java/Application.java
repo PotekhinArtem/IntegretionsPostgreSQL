639bf5bc22e2872d004b3f97e7f306639479f1f2
Artem Potekhin <potekhinartem@mail.ru>
2019-09-13 17:35:52 +0300

    New

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,18 +1,21 @@
+import java.io.*;
 import java.sql.*;
 
 public class Application {
-    public static void main(String[] args) throws SQLException {
+    File file = new File("newFile.txt");
+
+    public static void main(String[] args) throws SQLException, IOException {
         final String user = "postgres";
         final String password = "admin";
         final String url = "jdbc:postgresql://localhost:5432/demo";
-
         final Connection connection = DriverManager.getConnection(url, user, password);
-
+        Application appl = new Application();
+        PrintWriter pw = new PrintWriter(appl.file);
 
         try (PreparedStatement statement = connection.prepareStatement("SELECT a.aircraft_code, a.model, s.seat_no, s.fare_conditions, a.range\n" +
                 "FROM seats s JOIN aircrafts a ON s.aircraft_code = a.aircraft_code WHERE a.model LIKE '_%' ORDER BY a.range desc ")) {
-
-            System.out.println("aircraft_code\t\t model\t\t\3 seat_no\t fare_conditions\t range\t");
+            String tabl = "aircraft_code\t\t model\t\t\3 seat_no\t fare_conditions\t range\t";
+            pw.println(tabl);
             final ResultSet resultSet = statement.executeQuery();
 
             while (resultSet.next()) {
@@ -20,14 +23,11 @@ public class Application {
                 String byName =  resultSet.getString("model");
                 String byseat =  resultSet.getString("seat_no");
                 String byfare =  resultSet.getString("fare_conditions");
-                String byIndex =  resultSet.getString("range");
-                System.out.print("\t" +byCode +"\t\t");
-                System.out.print(byName +"\t\t\t");
-                System.out.print(byseat +"\t\t\f");
-                System.out.print(byfare +"\t\t\f");
-                System.out.println(byIndex + "\f");
+                Integer byIndex =  resultSet.getInt(5);
+                pw.printf( "|\t" + byCode + "\t|\t\3" + byName +"\t|\t\3" + byseat +"\t|\t\3" + byfare +"\t|\t" + byIndex + "|\n");
             }
         } finally {
+            pw.close();
             connection.close();
         }
     }
